42da6fcfeb6a0ab292b656ddd0868bce7764497d
王征 <alex.wz@alibaba-inc.com>
2021-01-19 14:39:14 +0800

    chap 02 done

---
+++ taco-cloud/src/main/java/com/alex/taco/tacocloud/OrderController.java
@@ -0,0 +1,30 @@
+package com.alex.taco.tacocloud;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.validation.Errors;
+import org.springframework.ui.Model;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+@RequestMapping("/orders")
+public class OrderController {
+ 	@GetMapping("/current")
+  	public String orderForm(Model model) {
+    	model.addAttribute("order", new Order());
+    	return "orderForm";
+  	}
+
+  	@PostMapping
+  	public String processOrder(@Valid Order order, Errors errors) {
+    	if (errors.hasErrors()) {
+      		return "orderForm";
+    	}
+    	log.info("Order submitted: " + order);
+    	return "redirect:/";
+  	}
+}
